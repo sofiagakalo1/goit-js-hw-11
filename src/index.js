59c454f6f1adb673dc936d6fc91832ef9d833b1f
sofiagakalo1 <sofiagakalo1@gmail.com>
2022-12-13 20:48:02 +0100

    f

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './sass/index.scss';
 import { markup } from './js/markup';
 import { refs } from './js/refs';
-import API from './js/api';
+import APIservice from './js/api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const APIservice = new API();
+const ApiService = new APIservice();
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.loadBtn.addEventListener('click', onLoadMoreBtn);
@@ -22,9 +22,9 @@ async function onFormSubmit(event) {
   }
   //----------------------------FETCHING OUR SEARCH OF PHOTOS-------------------------------
   try {
-    let photosData = await APIservice.fetchPosts();
+    let photosData = await ApiService.fetchPosts();
     //totalHits - The number of images accessible through the API.(500)
-    APIservice.NumberOfTotalPages(photosData.totalHits);
+    ApiService.NumberOfTotalPages(photosData.totalHits);
     //if there are no matches and backend return empty massive
     //(hits is a massive of objects(photos))the length of which is zero - give warning
     if (photosData.hits.length === 0) {
@@ -35,13 +35,13 @@ async function onFormSubmit(event) {
     }
     render(photosData);
     //---------CHECKING FOR ACTIVE OR NOT LOAD-MORE-BUTTON
-    if (APIservice.totalPages === 1) {
+    if (ApiService.totalPages === 1) {
       refs.loadBtn.classList.add('is-hidden');
-    } else if (APIservice.totalPages > 1) {
+    } else if (ApiService.totalPages > 1) {
       refs.loadBtn.classList.add('is-hidden');
     }
     //--------TELLING ABOUT SEARCH RESULTS
-    Notify.info(`Hooray! We found ${APIservice.NumberOfTotalPages} images.`);
+    Notify.info(`Hooray! We found ${ApiService.NumberOfTotalPages} images.`);
   } catch (error) {
     Notify.failure('Something went wrong [*_*]');
   }
@@ -57,5 +57,5 @@ function clearSearchResults() {
   //clearing markup of gallery results
   refs.galleryList.innerHTML = '';
   //start new searches from 1(first) page
-  APIservice.resetPage();
+  ApiService.resetPage();
 }
